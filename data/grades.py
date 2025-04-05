a80953c0656c551a2d8b73cebc2b5671b453d0c3
MatinT-81 <matintahani5@gmail.com>
2025-04-05 11:03:52 +0330

    initial data added

---
+++ data/grades.py
@@ -0,0 +1,4 @@
+grades = {
+    "9": {"Math": 4, "Science": 3, "History": 2},  # درس‌ها و تعداد ساعات هفتگی
+    "10": {"Physics": 3, "Chemistry": 3, "Literature": 2},
+}
---
+++ data/teachers.py
@@ -0,0 +1,4 @@
+teachers = {
+    "Mr. A": {"available_times": ["Saturday 08:00-09:00", "Monday 10:00-11:00"], "subjects": ["Math", "Physics"]},
+    "Ms. B": {"available_times": ["Sunday 09:00-10:00", "Tuesday 11:00-12:00"], "subjects": ["Science", "Chemistry"]},
+}
